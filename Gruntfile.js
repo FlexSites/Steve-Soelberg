9393a3a727bcb1a23c61d65b8f42d8586b98459a
Seth Tippetts <sethtippetts@gmail.com>
2014-10-26 17:44:15 -0600

    Moved Grunt processes to global folder. Removed old "critical.js" code from global.ng

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,230 +1,37 @@
+// Default Gruntfile.js
+
 var _ = require('lodash');
+var path = require('path');
 
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            local: {
-                options: {
-                    style: 'expanded'
-                },
-                files: {
-                    'public/css/style.css': ['public/scss/style.scss']
-                }
-            },
-            dev: {
-                options: {
-                    style: 'expanded'
-                },
-                files: {
-                    'public/css/style.css': ['public/scss/style.scss']
-                }
-            },
-            prod: {
-                options: {
-                    style: 'compressed'
-                },
-                files: {
-                    'public/css/style.css': ['public/scss/style.scss']
-                }
-            }
-        },
-        concat: {
-            local: {
-                src: [
-                    '../global/partials/ngHead.html',
-                    'index.html',
-                    '../global/partials/foot.html'
-                ],
-                dest: 'public/index.html'
-            },
-            dev: {
-                src: [
-                    '../global/partials/ngHead.html',
-                    'index.html',
-                    '../global/partials/foot.html'
-                ],
-                dest: 'public/index.html'
-            },
-            prod: {
-                src: [
-                    '../global/partials/ngHead.html',
-                    'index.html',
-                    '../global/partials/foot.html'
-                ],
-                dest: 'public/index.html'
-            }
-        },
-        // Uglify for development JS combination
-        uglify: {
-            local: {
-                options: {
-                    mangle: false,
-                    compress: false,
-                    beautify: true
-                },
+    // Load site configuration for build processes
+    var config = _.extend({routes: JSON.stringify(require('./routes.json'))}, require('./config.json'), require('./build.json'));
 
-                files: {
-                    'public/ng/global.js': [
-                        '../global/public/ng/lib/angular.min.js',
-                        '../global/public/ng/lib/angular-route.min.js',
-                        'public/ng/global.js'
-                    ]
-                }
-            },
-            dev: {
-                options: {
-                    mangle: false,
-                    compress: false,
-                    beautify: true
-                },
+    // Remove aliases property. Breaks grunt and it's not needed for building front-end
+    delete config.aliases;
 
-                files: {
-                    'public/ng/global.js': [
-                        '../global/public/ng/lib/angular.min.js',
-                        '../global/public/ng/lib/angular-route.min.js',
-                        'public/ng/global.js'
-                    ]
-                }
-            },
-            prod: {
-                options: {
-                    mangle: true,
-                    compress: true,
-                    beautify: false
-                },
+    require('load-grunt-config')(grunt, {
+        
+        // path to task.js files, defaults to grunt dir
+        configPath: path.join(process.cwd(), '../global/grunt'),
 
-                files: {
-                    'public/ng/global.js': [
-                        '../global/public/ng/lib/angular.min.js',
-                        '../global/public/ng/lib/angular-route.min.js',
-                        'public/ng/global.js'
-                    ]
-                }
-            }
-        },
-        // Closure compiler for production JS minification
-        'closure-compiler': {
-            global: {
-                closurePath: '/comedian/app',
-                js: [
-                    '../global/public/ng/lib/angular.min.js',
-                    '../global/public/ng/lib/angular-route.min.js',
-                    'public/ng/global.js'
-                ],
-                jsOutputFile: 'public/ng/min/global.js',
-                maxBuffer: 500,
-                options: {
-                    // compilation_level: 'ADVANCED_OPTIMIZATIONS',
-                    // language_in: 'ECMASCRIPT5_STRICT'
-                }
-            }
+        // auto grunt.initConfig
+        init: true,
+
+        // data passed into config.  Can use with <%= test %>
+        data: config,
+
+        // can optionally pass options to load-grunt-tasks.
+        // If you set to false, it will disable auto loading tasks.
+        loadGruntTasks: {
+            pattern: 'grunt-*',
+            config: require('./package.json'),
+            scope: 'devDependencies'
         },
 
-        watch: {
-            dev: {
-                tasks: ['uglify', 'sass:dev', 'concat:prod', 'hogan'],
-                files: [
-                    'public/scss/style.scss',
-                    'public/scss/partials/*.scss',
-                    'public/ng/*.js',
-                    'public/js/*.js',
-                    'config.json',
-                    'routes.json'
-                ],
-                options: {
-                    atBegin: true,
-                    spawn: false,
-                    debounceDelay: 250,
-                    livereload: 35731
-                }
-            }
-        }
-    });
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-closure-compiler');
-
-
-    //grunt.registerTask('default',['watch:actionItems']);
-
-    grunt.registerTask('build', 'Run all my build tasks.', function(env) {
-        if (env && (env == 'prod' || env == 'dev' || env == 'local')) {
-            grunt.task.run('sass:' + env, 'concat:' + env, 'hogan:' + env, 'uglify:' + env);
-        } else {
-            grunt.warn('Build environment invalid. Must be prod, dev, or local.');
-        }
-    });
-
-    // For adding build number to file names
-    function formatResource(src, buildNumber){
-        if(_.isString(src))return /^\/[^/]/.test(src)&&_.isNumber(buildNumber)?src.replace(/(\D)\.(js|css)/,'$1'+buildNumber+'.$2'):src;
-        if(_.isArray(src)){
-            _.each(src,function(resource,index){
-                src[index] = formatResource(resource, buildNumber);
-            });
-            return src;
-        }
-    }
-
-    // For compiling hogan.js/mustache 
-    function hoganRender(env, done) {
-
-        var hogan = require('hogan.js');
-        var clc = require('cli-color');
-        var fs = require('fs');
-        var async = require('async');
-
-        var filenames = [{
-            src: 'public/index.html',
-            dest: 'public/index.html'
-        }, {
-            src: 'public/ng/src/global.js',
-            dest: 'public/ng/global.js'
-        }];
-        var config = _.extend({}, require('./config.json'), require('./build.json'));
-
-        config.styles = formatResource(config.styles,config.buildNumber);
-        config.scripts = formatResource(config.scripts,config.buildNumber);
-
-        config.routes = JSON.stringify(require('./routes.json'));
-        config.env = env;
-
-        async.each(filenames, function(file, callback) {
-            fs.readFile(file.src, 'utf8', function(err, data) {
-                var tmpl = hogan.compile(data, {
-                    delimiters: '<@ @>'
-                });
-                fs.writeFile(file.dest, tmpl.render(config), function(err, data) {
-                    if (err) {
-                        throw new Error('Failed to save ' + file.dest)
-                    } else {
-                        callback();
-                    }
-                });
-            });
-        }, function(err) {
-            fs.writeFile('./build.json', JSON.stringify({
-                buildNumber: ++config.buildNumber
-            }), done);
-        });
-
-    }
-
-    grunt.registerTask('hogan:local', 'Compiles mustache syntax templates', function() {
-        hoganRender('local', this.async());
-    });
-    grunt.registerTask('hogan:dev', 'Compiles mustache syntax templates', function() {
-        hoganRender('test', this.async());
-    });
-    grunt.registerTask('hogan:prod', 'Compiles mustache syntax templates', function() {
-        hoganRender('', this.async());
+        //can post process config object before it gets passed to grunt
+        postProcess: function(config) {}
     });
 
 };
--- public/ng/src/global.js
+++ public/ng/src/global.js
@@ -76,7 +76,6 @@ var app = angular.module('comedian', ['ngRoute'])
             // $rootScope.bgClass = 'bg-home';
             $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
                 if (current) {
-                    console.log(current);
                     document.title = current.$$route.title;
                     $rootScope.bgClass = current.$$route.bgClass;
                 }
@@ -84,106 +83,10 @@ var app = angular.module('comedian', ['ngRoute'])
         }
     ]);
 
-
-    // $(function(){
-    //     $(document).on('click','#menu',function(){
-    //         $(this).toggleClass('open');
-    //     });
-    //     var img = new Image();
-    //     img.onload = function(){
-    //         $('html').css('background-image','url('+img.src+')');
-    //     };
-    //     img.src = "/media/large/contact.jpg";
-    // });
-/********************
- * CRITICAL PATH GLOBAL SCRIPT
- * Script that page loading and other scripts depend on
- ********************/
-
-//Generic type validation
-(function(w){
-    var objectTypes = ['String','Number','Undefined','Object','Function','Boolean'],
-        callback = function(type){
-            w['is'+type] = function(x){
-                return typeof x===type.toLowerCase();
-            };
-        };
-    for(var i = objectTypes.length;i--;){
-        callback(objectTypes[i]);
-    }
-}(window));
-
-//String validation
-function isArray(arr){
-    return isObject(arr)&&Array.isArray(arr);
-}
-function isURL(url){
-    return isString(url)&&/^(https?:\/\/|\/)/.test(url);
-}
-
 //Helper Methods
 function define(variable,defaultValue){
     if(isUndefined(this[variable]))this[variable] = isUndefined(defaultValue)?'':defaultValue;
 }
 function slugify(str){
     return (str||'').replace(/[^a-zA-Z0-9]+/g,'-').toLowerCase();
-}
-
-define('analytics','UA-42233857-2');
-define('_plugins',[]);
-define('noop',function(){});
-
-//Script & Stylesheet Loader
-(function(w,d){
-    var body = d.getElementsByTagName('body')[0],critical = d.getElementById('critical-js'),styles = d.getElementById('styles-css')
-    w.loadJS = function(src,callback){
-        loadFile('script',src,callback);
-    };
-    w.loadCSS = function(src){
-        loadFile('link',src);
-    };
-    w.load = function(file,callback){
-        if(!isString(file))for(var i = 0;i<file.length;i++)load(file[i],callback);
-        else if(_plugins[file])load(_plugins[file],callback);
-        else file.split('.').pop()==='js'?loadJS(file,callback):loadCSS(file,callback);
-    };
-    function loadFile(type,src,callback){
-        var isScript = type==='script',elem = d.createElement(type),listener;
-        if(d.getElementById(elem.id = slugify(src.replace(/.*[/]/,''))))return callback&&callback(src);
-        if(isScript){
-            elem.defer = elem.async = elem.src = formatResource(src);
-            if(isFunction(callback)){
-                elem.addEventListener('load',listener = function(){
-                    callback(src);
-                    elem.removeEventListener('load',listener,false);
-                },false);
-            }
-        }
-        else {
-            elem.href = src;
-            elem.type = 'text/css';
-            elem.rel = 'stylesheet';
-        }
-        body.insertBefore(elem,isScript?critical:styles);
-    }
-}(window,document));
-
-//Google Analytics
-if(analytics){
-    (function(a,b){
-        a.GoogleAnalyticsObject = b;
-        a[b] = a[b]||function(){
-            (a[b].q = a[b].q||[]).push(arguments);
-        };
-        a[b].l = 1*new Date;
-        load(['//www.google-analytics.com/analytics.js','//www.google-analytics.com/plugins/ua/linkid.js']);
-    })(window,"ga");
-    
-    ga('create',analytics,location.hostname);
-    ga('require','displayfeatures');
-    ga('require','ecommerce','ecommerce.js');
-    ga('send','pageview');
-}
-else {
-    window.ga = noop;
 }
