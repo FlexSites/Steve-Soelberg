17b66592b11c906e7fe2b4b3f0fb520d9c43e11a
Seth Tippetts <sethtippetts@gmail.com>
2014-10-30 19:36:50 -0600

    Updated global grunt build tasks to be grunt.loadTasks vs 3rd party plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,11 @@
 // Default Gruntfile.js
 
-var _ = require('lodash');
-var path = require('path');
-
 module.exports = function(grunt) {
 
-    // Load site configuration for build processes
-    var config = _.extend({routes: JSON.stringify(require('./routes.json'))}, require('./config.json'), require('./build.json'));
-
-    // Remove aliases property. Breaks grunt and it's not needed for building front-end
-    delete config.aliases;
-
-    require('load-grunt-config')(grunt, {
-        
-        // path to task.js files, defaults to grunt dir
-        configPath: path.join(process.cwd(), '../global/grunt'),
-
-        // auto grunt.initConfig
-        init: true,
-
-        // data passed into config.  Can use with <%= test %>
-        data: config,
-
-        // can optionally pass options to load-grunt-tasks.
-        // If you set to false, it will disable auto loading tasks.
-        loadGruntTasks: {
-            pattern: 'grunt-*',
-            config: require('./package.json'),
-            scope: 'devDependencies'
-        },
-
-        //can post process config object before it gets passed to grunt
-        postProcess: function(config) {}
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        buildPath: __dirname.split('/').pop()
     });
 
+    grunt.loadTasks('../global/grunt');
 };
