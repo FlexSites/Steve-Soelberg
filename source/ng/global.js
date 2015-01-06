8f80be13fcedb4c1c946bfb1c4c417339a15ed3a
Seth Tippetts <sethtippetts@gmail.com>
2015-01-05 23:08:28 -0700

    Added 3rd testimonial

--- source/ng/global.js
+++ source/ng/global.js
@@ -1,7 +1,10 @@
 angular.module('app')
-    .controller('HomeCtrl', ['$scope', '$interval', function($scope, $interval){
-        var testimonials = [
-        {
+    .controller('HomeCtrl', ['$scope', '$interval', function($scope, $interval) {
+        var testimonials = [{
+            quote: "I've never laughed so hard in my life, whether it's the first time I've heard him or the hundredth.",
+            author: "Jesse Ranney",
+            company: "Producer for Paradise Road Productions"
+        }, {
             quote: "Work Christmas party, booked Steve Soelberg for our entertainment, he is amazing! Anyone that can make a room full of accountants laugh has some mad skills! Thank you Steve!",
             author: "Wendy Duke",
             company: "Harman Management Corp"
@@ -9,12 +12,13 @@ angular.module('app')
             quote: "Steve Soelberg is hilarious! Great timing. Great comedic insights. Everyone on my team laughed and smiled for an hour straight.... just what I had hoped for. I look forward to seeing Steve's act again.",
             author: "Michael Janda",
             company: "CEO of RiSER"
-        }
-        ];
+        }];
         var i = 0;
         $scope.testimonial = testimonials[i];
-        $interval(function(){
-            console.log(testimonials[i % 2]);
-            $scope.testimonial = testimonials[++i % 2];
+        $interval(function() {
+            $scope.testimonial = testimonials[++i];
+            if (i === testimonials.length - 1) {
+                i = -1;
+            }
         }, 7500);
     }]);
