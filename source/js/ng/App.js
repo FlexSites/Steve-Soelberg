cc0f12611d252b3ff430482c2e476e135c413eb4
Seth Tippetts <sethtippetts@gmail.com>
2015-11-26 01:46:59 -0700

    Remove the local override for the API

--- source/js/ng/App.js
+++ source/js/ng/App.js
@@ -1,6 +1,5 @@
 angular.module('SteveSoelberg', ['ui.router', 'FlexSites']);
 
 angular.module('FlexSites')
-  .value('apiBase', 'http://localapi.flexsites.io/api/v1')
   .value('siteToken', '54d02511b0af1be7a83bb48e');
 
